13e08eb48dd4ac8051da5091ec504371c2fc1aa8
Georgi Matsov <georgi.macov@gmail.com>
2015-05-19 11:33:34 +0300

    Added static background, play button, score label.

--- DolditMan/DolditMan/Program.cs
+++ DolditMan/DolditMan/Program.cs
@@ -16,7 +16,7 @@ namespace DolditMan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DolditMan());
         }
     }
 }
